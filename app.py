dc4d45ed9549a0e482272777af37cff5efc046c7
torqu3e <teji@torqu3e.net>
2020-12-19 18:06:19 +0000

    Flask interface initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api
+from resources import SourceFile
+
+
+app = Flask(__name__)
+# app.config["PROPAGATE_EXCEPTIONS"] = True
+
+
+api = Api(app)
+
+api.add_resource(SourceFile, "/")
+
+if __name__ == "__main__":
+    app.run()
---
+++ resources.py
@@ -0,0 +1,59 @@
+import os
+from datetime import datetime
+from flask import flash, request, url_for, make_response, send_file
+from flask_restful import Resource
+from werkzeug.utils import secure_filename
+from speedangle_2_racechrono import (
+    read_speedangle_file,
+    speedangle_to_racechrono_vbo,
+    write_racechrono_file,
+)
+
+ALLOWED_EXTENSIONS = {"txt", "log", "sa"}
+UPLOAD_FOLDER = "/tmp/"
+
+
+class SourceFile(Resource):
+    def post(self):
+
+        if "file" not in request.files:
+            return {"message": "No file provided"}, 400
+        file = request.files["file"]
+
+        if file.filename == "":
+            return {"message": "Blank filename received"}, 400
+
+        if file and not self.allowed_file(file.filename):
+            return {"message": "Unrecognized file type"}, 400
+
+        filename = secure_filename(file.filename)
+        file.save(UPLOAD_FOLDER + filename)
+
+        output_filename = (
+            filename.rsplit(".", 1)[0] + "_" + datetime.now().strftime("%s") + ".vbo"
+        )
+
+        timestamp, line_data = read_speedangle_file(UPLOAD_FOLDER + filename)
+        rc_lines = speedangle_to_racechrono_vbo(timestamp, line_data)
+
+        write_racechrono_file(rc_lines, UPLOAD_FOLDER + output_filename)
+
+        return send_file(UPLOAD_FOLDER + output_filename, as_attachment=True)
+
+    def get(self):
+        headers = {"Content-Type": "text/html"}
+        html = """
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+            <input type=file name=file>
+            <input type=submit value=Upload>
+        </form>
+        """
+        return make_response(html, 200, headers)
+
+    def allowed_file(self, filename):
+        return (
+            "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+        )
